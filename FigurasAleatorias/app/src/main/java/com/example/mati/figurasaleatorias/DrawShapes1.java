a568646edc207f96a3833543e21c085622fce60c
csobrino <dam.csobrino@institutoserpis.org>
2016-11-14 11:31:28 +0100

    Añadido FigurasAleatorias v1

---
+++ FigurasAleatorias/app/src/main/java/com/example/mati/figurasaleatorias/DrawShapes1.java
@@ -0,0 +1,21 @@
+package com.example.mati.figurasaleatorias;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DrawShapes1 extends Activity {
+    private RandomShapeView mDrawingArea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_shape1);
+        mDrawingArea = (RandomShapeView)findViewById(R.id.drawing_area);
+    }
+/** Handless events for the button. Redraws the ShapeView. */
+    public  void redraw(View clickedButton){
+        mDrawingArea.invalidate();
+    }
+}
